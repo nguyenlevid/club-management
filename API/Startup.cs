d521552d57869abfb3dbca6f4883fc1c09994793
nguyenleviet <85124005+nguyenlevid@users.noreply.github.com>
2022-01-22 21:42:06 -0600

    Added files

--- API/Startup.cs
+++ API/Startup.cs
@@ -54,6 +54,9 @@ namespace API
             
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
